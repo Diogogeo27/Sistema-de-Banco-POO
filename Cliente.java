469fea72a36e31929b63bd58f9791bf63f0de170
Diogogeo27 <153551119+Diogogeo27@users.noreply.github.com>
2026-05-07 21:59:00 -0300

    Refactor Cliente class for password access and authentication
    Added getSenha method and moved autenticar method.

--- Cliente.java
+++ Cliente.java
@@ -1,28 +1,28 @@
 public class Cliente {
-
     private String nome;
     private String cpf;
     private String senha;
 
     public Cliente(String nome, String cpf, String senha) {
-
         this.nome = nome;
         this.cpf = cpf;
         this.senha = senha;
     }
 
-    public boolean autenticar(String senha) {
-
-        return this.senha.equals(senha);
-    }
-
     public String getNome() {
-
         return nome;
     }
 
     public String getCpf() {
-
         return cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    // 🔐 Método de autenticação
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
     }
 }
